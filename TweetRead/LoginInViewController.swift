3683582b919800b28d2e6fef94556e26088a7d49
Sean Livingston <sean.d.livingston@gmail.com>
2017-06-03 23:27:45 -0400

    UIStackView now embedded in UIScroll View

---
+++ TweetRead/LoginInViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginInViewController.swift
+//  TweetRead
+//
+//  Created by Sean on 5/30/17.
+//  Copyright © 2017 Sean. All rights reserved.
+//
+
+import UIKit
+
+class LoginInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
